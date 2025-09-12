d9ae07fd060152d9cdd2a07c6137dfeb9407dbab
Md. Abu Bakkar Siddiq <siddiq.aiub@gmail.com>
2025-09-12 20:03:47 +0600

    add herosection

--- src/HeroSection.jsx
+++ src/HeroSection.jsx
@@ -1,7 +1,7 @@
 export default function HeroSection(){
     return (
-        <div className="div">
-            dsakkj
+        <div className="div pt-30 mx-auto flex justify-center align-center">
+            <div className="card gap-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente labore dignissimos exercitationem fugiat facere voluptate, corporis quis libero quae amet quam, eum qui temporibus quaerat aspernatur molestias delectus impedit minima?</div>
         </div>
     );
 }
